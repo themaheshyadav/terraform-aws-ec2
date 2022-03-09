b4f74c2e2b2d5f689a16e7f8d6e4c09a23d93aef
Mahesh Yadav <85216933+themaheshyadav@users.noreply.github.com>
2022-03-09 23:17:13 +0530

    Update example.tf

--- _example/basic_example/example.tf
+++ _example/basic_example/example.tf
@@ -45,7 +45,7 @@ module "keypair" {
   source  = "clouddrove/keypair/aws"
   version = "0.15.0"
 
-  public_key      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLbkgVCxhs3oI0Cz/0AEjAQ+/wZryh/C9mcDEMraRO/HYCSR4CkALDklRUL0bejf8Jfdi58Poakox55bBfoAIA/9AR8F6mK7iyLigyTT1VJGBqqaczCJAcW1Cd2dcIGAxyNaxl8FhAMIh69MRYKZqVi1SC73RTd3v4YZW2F6OGz6WlPnQrL2E0iLmybiH1qJH9zt3VIYOxhlw8xB2EwHNGqxFU9amTBsN+jhUHHvcdLGqMQ3sGxfvLymaesX/a/MR+Au7c6MFCQ1T1mxkBFfDgsRQNLvKW3o8U8T2tGJ15G24SvTvVwAKJSu0WkwwIAW+KF8RvJj1n1P1Rj+VXQSo4V0O2omd33OlzZmAucGKQF1l48uya9Im9n5Zmqk6UxhESBIP6G8GI/FduGpPwOm/xjxalmBilZ1RnIf96QwMnoRbZsS6rE1371FqFJwh6A/TEX+m0wHsGu/Sg1V8jVEe64AEGGUy9maWPMpMB9PqnnzIs4X9ndmuKkTqZbhUMV7M= mahesh@mahesh"
+  public_key      = "jhUHHvcdLGqMQ3sGxfvLymaesX/a/MR+Au7c6MFCQ1T1mxkBFfDgsRQNLvKW3o8U8T2tGJ15G24SvTvVwAKJSu0WkwwIAW+KF8RvJj1n1P1Rj+VXQSo4V0O2ouyk55md33OlzZmAucGKQF1l48uya9Im9n5Zmqk6UxhESBIP6G8GI/FduGpPwOm/xjxalmBilZ1RnIf96QwMnoRbZsS6rE1371FqFJwh6A/TEX+m0wHsGu/Sg1V8jVEe64AEGGUy9maWPMpMB9PqnnzIs4X9ndmuKkTqZbhUMV7M= mahesh@mahesh"
   key_name        = "devops"
   environment     = "test"
   label_order     = ["name", "environment"]
